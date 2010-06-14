8648c9e911291831146f516ad0f75279505b227d
Hanyoung Cho <rumidier@gmail.com>
2010-06-14 22:12:40 +0900

    Process command line input and print them

--- sort/sort.c
+++ sort/sort.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int
 main (int    argc,
       char **argv)
 {
+  int i;
+
   if (argc < 2)
     {
       printf ("Usage: %s <num1> [<num2> <num3> ...]\n", argv[0]);
+      exit (EXIT_FAILURE);
     }
+
+  for (i = 1; i < argc; ++i)
+    {
+      printf ("%s ", argv[i]);
+    }
+  printf ("\n");
 
   return 0;
 }

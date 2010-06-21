a0a5889eede25a377f1f8cf4d6fb2da9f692e9a9
Hanyoung Cho <rumidier@gmail.com>
2010-06-21 21:51:53 +0900

    Print contents which is grabbed via stdin with line no.

--- exam/stdio/line-counter.c
+++ exam/stdio/line-counter.c
@@ -3,7 +3,21 @@
 int
 main (int argc, char *argv[])
 {
-  printf ("Hello!\n");
+  char line [100];
+  int line_num = 1;
+
+  while (1)
+    {
+      fgets (line, sizeof (line), stdin);
+
+      if (feof (stdin))
+        {
+          break;
+        }
+
+      printf ("%6d  %s", line_num, line);
+      line_num++;
+    }
 
   return 0;
 }

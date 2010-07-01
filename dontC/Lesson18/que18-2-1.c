3f65cb8365871b1f9df8dd3238b76528691f26f8
Hanyoung Cho <rumidier@gmail.com>
2010-07-01 20:35:53 +0900

    Lesson18 que18-2-1 p442

--- dontC/Lesson18/que18-2-1.c
+++ dontC/Lesson18/que18-2-1.c
@@ -4,7 +4,16 @@ int
 main (int   argc,
       char *argv[])
 {
-  scanf ("%s", input);
+  char str[100];
+  int len = 0;
 
+  printf ("문자열 입력 : ");
+  scanf ("%s", str);
 
+  while (str[len] != '\0')
+    len++;
+
+  printf ("문자열의 길이는 %d \n", len);
+
+  return 0;
 }

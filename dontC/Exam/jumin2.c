4a85b7bf81bb47ced25e001d1c4b33a4a362b9d9
Keedi Kim <keedi.k@gmail.com>
2010-07-12 21:44:06 +0900

    Add another new skeleton code for p.503, ex 6

---
+++ dontC/Exam/jumin2.c
@@ -0,0 +1,17 @@
+/**
+ * 주민등록번호 확인 프로그램
+ *
+ *   난 정말 C Programming을 공부한 적이 없다구요!
+ *   p.503, 문제 6
+ */
+
+#include <stdio.h>
+
+int
+main (int   argc,
+      char *argv[])
+{
+  printf ("Hello World!\n");
+
+  return 0;
+}

417d2ad1a0f6d8acd0c1941974945153994fd40c
Hanyoung Cho <rumidier@rumidier-HP-Pavilion-dm1-Notebook-PC.(none)>
2011-05-16 18:02:36 +0900

    Delete Start_vi

--- Start_vi/P_29.c
+++
@@ -1,6 +0,0 @@
-The 6th edition oF <citetitle>Learning the vi Edito</citetitle>
-bring the BOOK INTO The late 1990S.
-In particulAR, BESIDES the &amp; ldquo; original&amp;rdquo; version of
-<command>vi<COMMAND> that comes ad a standard part of every UNIX system,
-  there are nOW A NUMBER of freely available &amp;ldquo;clones&amp;rdquo;
-or work-alike editors.

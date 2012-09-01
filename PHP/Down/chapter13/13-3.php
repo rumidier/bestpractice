ff6398a2a3fa51c4a87e2fb0da3fd220a7068fd7
Hanyoung Cho <rumidier@gmail.com>
2012-09-01 17:36:58 +0900

    I don't know delete..

--- PHP/Down/chapter13/13-3.php
+++
@@ -1,9 +0,0 @@
-<?
-	if (empty($_FILES['upfile']['name']))
-	{
-		echo "업로드된 파일이 없습니다.";
-		exit;
-	}
-	move_uploaded_file($_FILES['upfile']['tmp_name'], './uploads_dir/' . $_FILES['upfile']['name']);
-	echo "파일이 업로드 되었습니다.";
-?>
